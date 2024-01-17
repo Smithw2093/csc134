6b0fa072ee95bbdfcb2b2ffc0a71f9739eb8d7a1
Smithw2093 <156947934+Smithw2093@users.noreply.github.com>
2024-01-17 19:37:20 +0000

    M1T1 done

---
+++ M1T1_Smith.cpp
@@ -0,0 +1,22 @@
+// comment
+/* 
+CSC 134
+M1T1 - Hello World  
+B Smith 
+1/17/24
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    string pokemon = "Greninja";
+    cout << "My favorite Pokemon is " << pokemon << endl;
+    cout << endl << endl;
+    cout << "what's yours? ";
+    string answer;
+    cin >> answer;
+    cout << "You said " << answer << endl;
+    cout << "I guess that's OK." << endl << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # csc134
 CSC 135 C++
+
+Brent / Greninja / Shelby Gt500 
