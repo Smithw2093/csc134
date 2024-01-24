ca234f04edf39831b09020396b882410d249087b
Smithw2093 <156947934+Smithw2093@users.noreply.github.com>
2024-01-24 13:14:27 -0500

    Update M1HW1_Smith.cpp

--- M1HW1_Smith.cpp
+++ M1HW1_Smith.cpp
@@ -12,5 +12,8 @@ int main () {
     string name = "2012"
     double box_office = 1.519 
 
-
+cout << "The movie " << movie_name << "came out in " << movie_year << ", and had a worldwide gross of around " << box_office << " billion 
+dollars." <<endl;
+cout << "There's no throne, there is no version of this where you come out on top. Maybe your army comes and maybe it's too much 
+for us but it's all on you. Because if we can't protect the Earth, you can be damned well sure we'll avenge it!" << endl;
 
